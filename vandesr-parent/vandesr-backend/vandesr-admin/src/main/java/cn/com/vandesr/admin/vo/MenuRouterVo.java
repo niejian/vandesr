e147c213acbffb67c3dcf52ae998df41e9d72984
聂剑 <niejian9001@163.com>
2019-07-19 17:10:07 +0800

    登录成功返回菜单信息

---
+++ vandesr-parent/vandesr-backend/vandesr-admin/src/main/java/cn/com/vandesr/admin/vo/MenuRouterVo.java
@@ -0,0 +1,21 @@
+package cn.com.vandesr.admin.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author: nj
+ * @date: 2019-07-19:15:55
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuRouterVo implements Serializable {
+    private String path;
+    private String title;
+}
